32b6a32393c65b36273d357212bb1dfb2d452a32
Terran Olson <terran@halfcadence.net>
2016-03-15 19:50:41 -0700

    updating tests

--- starseeker/card/tests.py
+++ starseeker/card/tests.py
@@ -6,6 +6,10 @@ class CardTestCase(TestCase):
 
     fixtures = (
         'card/fixtures/card_cardblock.json',
+        'card/fixtures/card_cardmechanic.json',
+        'card/fixtures/card_heroclass.json',
+        'card/fixtures/card_tribe.json',
+        'card/fixtures/card_card.json',
     )
 
     def test_minion_attributes(self):
@@ -15,15 +19,15 @@ class CardTestCase(TestCase):
         self.assertEqual(card.attack, 4)
         self.assertEqual(card.health, 5)
         self.assertEqual(card.rarity, Card.Rarity.COMMON)
-        self.assertEqual(card.card_type, Card.Type.MINION)
+        self.assertEqual(card.card_type, Card.CardType.MINION)
         self.assertEqual(card.collectible, True)
-        self.assertIsNone(card.effect)
+        self.assertFalse(card.effect)
 
 
     def test_minion_references(self):
         card = Card.objects.get(card_id='EX1_319')
 
-        block = Block.objects.get(name='Classic')
+        block = CardBlock.objects.get(name='Classic')
         self.assertEqual(card.block.pk, block.pk)
 
         hero_class = HeroClass.objects.get(name='Warlock')
@@ -32,13 +36,8 @@ class CardTestCase(TestCase):
         tribe = Tribe.objects.get(name='Demon')
         self.assertEqual(card.tribe.pk, tribe.pk)
 
-        has_mechanic = False
-        card_mechanic = CardMechanic.objects.get(name='Battlecry')
-        for mechanic in card.mechanics:
-            if mechanic.pk == card_mechanic.pk:
-                has_mechanic = True
-                break
-        self.assertTrue(has_mechanic)
+        self.assertTrue(card.mechanics.filter(name='Battlecry').exists())
+        self.assertFalse(card.mechanics.filter(name='Charge').exists())
 
 
     def test_weapon_attributes(self):
@@ -48,6 +47,6 @@ class CardTestCase(TestCase):
         self.assertEqual(card.attack, 5)
         self.assertEqual(card.health, 2)
         self.assertEqual(card.rarity, Card.Rarity.EPIC)
-        self.assertEqual(card.type, Card.Type.WEAPON)
+        self.assertEqual(card.card_type, Card.CardType.WEAPON)
         self.assertEqual(card.collectible, True)
         self.assertTrue(len(card.effect) > 0)
